a1e519c7487fe1b96cb15ffed1d80c506408e2e3
gallais <guillaume.allais@ens-lyon.org>
2017-08-02 09:35:10 +0200

    [ doc ] index for the Safe subsection

--- index.sh
+++ index.sh
@@ -1,3 +1,8 @@
-for i in $( find src -name "*.agda" | grep -v "index.agda" | sed 's/src\/\(.*\)\.agda/\1/' | sed 's/\//\./g' | sort ); do
-  echo "import $i" >> src/index.agda;
+#/bin/bash
+if [ "$1" ] && [ ${1} != "" ]
+then
+  echo -e "module $1.index where\n" > src/${1}/index.agda
+fi
+for i in $( find src/${1} -name "*.agda" | grep -v "index.agda" | sed 's|src/\(.*\)\.agda|\1|' | sed 's|/|.|g' | sort ); do
+  echo "import $i" >> src/${1}/index.agda;
 done
--- src/index.agda
+++ src/index.agda
@@ -6,4 +6,8 @@ import README
 -- For a brief presentation of every single module, head over to
 import Everything
 
+-- For a list of the subsection of the library compatible with the --safe
+-- command line option, you can read the following index:
+import Safe.index
+
 -- Otherwise, here is an exhaustive, stern list of all the available modules:
